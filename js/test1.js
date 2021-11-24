5a73ab9366ba191e317713a0c296bc4152caa977
DonnyHarmaizi <donnypart2@gmail.com>
2021-11-24 16:16:23 +0700

    first commit

---
+++ js/test1.js
@@ -0,0 +1,12 @@
+// Expected Result = 55
+// Direction : Return sum of array
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function result(input) {
+  // Your Code Here
+  for(i = 0; i <input.length; i++){
+    total += input[i];
+console.log (total);
+console.log(result(input));
+  }
+}
